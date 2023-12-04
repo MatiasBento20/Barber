b4314916cfa4267190ee4869b074a0f286e77060
matiasbento20 <matiasbento20@gmail.com>
2023-12-03 23:54:35 -0300

    nueva version de proyecto

--- script.js
+++ script.js
@@ -87,7 +87,6 @@ function mostrarDatosCliente(data) {
     document.getElementById('fechanacCliente').textContent = `${data.FechaNacimiento}`;
     document.getElementById('telefonoCliente').textContent = `${data.Telefono}`;
     document.getElementById('emailCliente').textContent = `${data.Email}`;
-    document.getElementById('puntosActuales').textContent = `Puntos actuales: ${data.PuntosActuales}`;
     document.getElementById('puntosTotales').textContent = `Puntos totales: ${data.PuntosTotales}`;
 }
 
@@ -98,6 +97,9 @@ function guardarCorte() {
     var fecha = document.getElementById('fecha').value;
     var empleado = document.getElementById('empleado').value;
     var dni = document.getElementById('dni').value;
+    var sucursal = document.getElementById('sucursal').value;
+    var puntos = document.getElementById('puntos').value;
+
 
     var data = {
         monto: montoCobrado,
@@ -105,10 +107,10 @@ function guardarCorte() {
         fecha: transformarFecha(fecha),
         employeeid: empleado,
         dni: dni,
-        
+        sucursalId: sucursal,
+        puntosTotales: puntosTotales,
         
     };
-
     const config = {
         url: `https://cork-be.onrender.com/transactions`,
         method: 'POST',
@@ -128,10 +130,39 @@ function guardarCorte() {
             document.getElementById('detalle').value = '';
             document.getElementById('fecha').value = '';
             document.getElementById('empleado').value = '';
+            sumarPuntos(dni, monto);
+            alert(response.data.mensaje);
+        });
+      
+}
+
+function sumarPuntos(dni, monto) {
+    var puntos = monto/1000;
+
+    var data = {
+        puntos: puntos,
+        dni: dni,
+        
+    };
+    const config = {
+        url: `http://cork-be.onrender.com/puntos`,
+        method: 'POST',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+        },
+        data: data
+    };
+
+
+    axios(config)
+        .then(response => {
+            console.log(response.data);
             alert(response.data.mensaje);
         })
       
 }
+
 function buscarTransaccionPorDni() {
     var dni = document.getElementById('dni').value;
     const config = {
@@ -145,8 +176,8 @@ function buscarTransaccionPorDni() {
 
     axios(config)
         .then(response => {
-            var clienteData = response.data[0]; // Suponiendo que la respuesta contiene los datos del cliente
-            mostrarTransacciones(clienteData); // Llamar a la función para mostrar los datos en la página
+            var clienteData = response; // Suponiendo que la respuesta contiene los datos del cliente
+            mostrarTransacciones(clienteData.data); // Llamar a la función para mostrar los datos en la página
         })
         .catch(error => {
             // Manejar errores
@@ -154,9 +185,27 @@ function buscarTransaccionPorDni() {
         });
 }
 
-function mostrarTransacciones(data) {
-    document.getElementById('monto').textContent = `Monto Cobrado: ${data.monto}`;
-    document.getElementById('detalle').textContent = `Detalle: ${data.detalle}`;
-    document.getElementById('fecha').textContent = `Fecha: ${data.fecha}`;
-    
+function mostrarTransacciones(datas) {
+    const listaTransacciones = document.getElementById('listaTransacciones');
+
+    // Limpiamos el contenido previo de la lista
+    listaTransacciones.innerHTML = '';
+
+    // Obtener solo las últimas 5 transacciones
+    const ultimas5Transacciones = datas.slice(-3);
+
+    ultimas5Transacciones.forEach(data => {
+        const transaccionDiv = document.createElement('div');
+        transaccionDiv.classList.add('transaccion');
+
+        const transaccionText = document.createElement('p');
+        transaccionText.textContent = `Monto Cobrado: ${data.monto} - Detalle: ${data.detalle} - Fecha: ${data.fecha} - Puntos Totales: ${data.PuntosTotales}`;
+        transaccionDiv.appendChild(transaccionText);
+
+        listaTransacciones.appendChild(transaccionDiv);
+    });
+
+
 }
+
+
